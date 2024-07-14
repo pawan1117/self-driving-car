3deb45c7837ada9a98838766379adf3807bade2f
pawan1117 <155816784+pawan1117@users.noreply.github.com>
2024-07-14 10:59:46 +0530

    Update car.js

--- car.js
+++ car.js
@@ -6,7 +6,7 @@ class Car{
         this.height=height;
 
         this.speed=0;
-        this.acceleration=0.2;
+        this.acceleration=0.5;
         this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
